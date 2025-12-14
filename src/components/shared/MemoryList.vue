7fb80937eed1007a79897847b8352f747a0a6e9e
abbasian <abbasian@thn.com>
2025-12-14 22:32:38 +0330

    some change

--- src/components/shared/MemoryList.vue
+++ src/components/shared/MemoryList.vue
@@ -12,17 +12,17 @@ const { data, isLoading } = props.useQueryFn()
   <div>
     <div v-if="isLoading">Loading...</div>
 
-    <div v-else class="flex items-center flex-wrap gap-2">
+    <div v-else class="flex items-center flex-wrap gap-10">
       <div
         v-for="item in data"
         :key="item.id"
-        class="w-44 h-44 border rounded-md shadow-2xl p-2"
+        class="w-52 h-56 flex flex-col gap-4 rounded-md shadow-2xl p-4 bg-primary-100"
       >
-        <p>{{ item.user.name }}</p>
-        <p>{{ item.sport.name }}</p>
-        <p>{{ dayjs(item.date).format('YYYY/MM/DD') }}</p>
-        <p>{{ dayjs(item.date).format('HH:mm') }}</p>
-        <p>{{ item.mood === 0 ? 'happy' : 'sad' }}</p>
+        <p>name: {{ item.user.name }}</p>
+        <p>sport: {{ item.sport.name }}</p>
+        <p>date: {{ dayjs(item.date).format('YYYY/MM/DD') }}</p>
+        <p>time: {{ dayjs(item.date).format('HH:mm') }}</p>
+        <p>mood: {{ item.mood === 0 ? 'happy' : 'sad' }}</p>
       </div>
     </div>
   </div>
