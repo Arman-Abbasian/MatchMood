8b29e47917dbbd9d081466c444e1c43534160d2f
abbasian <abbasian@thn.com>
2025-12-16 20:12:34 +0330

    work on user panel statistics style

--- src/pages/user/User.vue
+++ src/pages/user/User.vue
@@ -1,18 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  ArrowLongLeftIcon,
+  ChartBarSquareIcon,
+  HomeIcon,
+  ListBulletIcon,
+  PlusIcon,
+  UserIcon,
+} from '@heroicons/vue/24/outline'
+</script>
 
 <template>
-  <div class="grid grid-cols-12 h-full bg-red-200">
-    <div class="col-span-3 bg-amber-200 overflow-auto">
-      <ul class="flex flex-col gap-8">
-        <li><router-link to="">statistics</router-link></li>
-        <li><router-link to="profile">profile</router-link></li>
-        <li><router-link to="addMemory">add Memory</router-link></li>
-        <li><router-link to="MemoryList">Memory list</router-link></li>
-        <li><router-link to="/">Home</router-link></li>
-        <li>logout</li>
+  <div class="grid grid-cols-12 h-full bg-red-200 text-white">
+    <div class="col-span-2 bg-primary-500 overflow-auto">
+      <ul class="flex flex-col gap-8 pl-3 py-4">
+        <li>
+          <router-link to=""
+            ><div class="flex items-center gap-2">
+              <chart-bar-square-icon class="w-6" />
+              <p>statistics</p>
+            </div></router-link
+          >
+        </li>
+        <li>
+          <router-link to="profile"
+            ><div class="flex items-center gap-2">
+              <user-icon class="w-6" />
+              <p>profile</p>
+            </div></router-link
+          >
+        </li>
+        <li>
+          <router-link to="addMemory"
+            ><div class="flex items-center gap-2">
+              <plus-icon class="w-6" />
+              <p>add memory</p>
+            </div></router-link
+          >
+        </li>
+        <li>
+          <router-link to="MemoryList"
+            ><div class="flex items-center gap-2">
+              <list-bullet-icon class="w-6" />
+              <p>memory list</p>
+            </div></router-link
+          >
+        </li>
+        <li>
+          <router-link to="/"
+            ><div class="flex items-center gap-2">
+              <home-icon class="w-6" />
+              <p>Home</p>
+            </div></router-link
+          >
+        </li>
+        <li>
+          <div class="flex items-center gap-2">
+            <arrow-long-left-icon class="w-6" />
+            <p>Logout</p>
+          </div>
+        </li>
       </ul>
     </div>
-    <div class="col-span-9 overflow-auto">
+    <div class="col-span-10 overflow-auto bg-primary-50">
       <router-view></router-view>
     </div>
   </div>
